119c560c32626da6d3c0da372ea995734db0d60c
KatRichardson <33345344+KatRichardson@users.noreply.github.com>
2017-12-04 09:26:36 +1300

    Create header.js

---
+++ src/components/headerComponent/header.js
@@ -0,0 +1,29 @@
+import React, { Component } from 'react';
+import {
+  Link
+} from 'react-router-dom';
+
+class Header extends Component {
+  render() {
+    return (
+      <header>
+
+	   	<head>
+        <script type="text/javascript" src="script.js"></script>
+  			<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css"/>
+		  </head>
+
+  			<div className="parent2">
+  				<div className="test1"><i className="fa fa-user fa-2x"></i></div>
+  				<div className="test2"><i className="fa fa-graduation-cap fa-2x"></i></div>
+ 				  <div className="test3"><i className="fa fa-code fa-2x"></i></div>
+  				<div className="test4"><i className="fa fa-envelope-o fa-2x"></i></div>
+  				<div className="mask2"><i className="fa fa-home fa-3x"></i></div>
+			</div>
+
+	   </header>
+    );
+  }
+}
+
+export default Header;
