187149989f3a4041e4d9282ea455a35222434785
KatRichardson <33345344+KatRichardson@users.noreply.github.com>
2017-12-05 17:34:28 +1300

    Update contactMe.js

--- src/components/pages/contactMe.js
+++ src/components/pages/contactMe.js
@@ -4,38 +4,55 @@ import {
 } from 'react-router-dom';
 
 class contactMe extends Component {
+  constructor(props) {
+    super(props)
+      this.state = {name: ''};
+      this.state = {email: ''};
+      this.state = {message: ''};
+
+      this.handleChange = this.handleChange.bind(this);
+      this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert("Thank you! I'll be in touch soon!");
+    event.preventDefault();
+  }
+
+
   render() {
     return (
       <div className="container-fluid">
-      	<h1>Contact Me</h1>
-        <h2>The Planning Process</h2>
+        <div className="heading">
+      	 <h1>Contact Me</h1>
+        </div>
 
-        <table>
-          <tr>
-            <td>
-              <h3>Situation Analysis</h3>
-              <p>At this current moment, Sipreme is practically unknown. There has been no promotional media from them,
-              nobody knows they exist. Sipreme supposedly sells health drinks that are taking the world by storm as they
-              are practically a meal in a bottle. Other companies such as Soylent (USA) and Aussielent (Australia)
-              provide these drinks, however either won't ship to New Zealand or charge practically double for the 
-              shipping. This is why Sipreme needs to up their game. They have the advantage of there being a high
-              demand for these products, however need to make themselves known.</p>
-            </td>
-            <td>
-              <h3>Mission, Vision, Goals & Objectives</h3>
-              <p>This is where we'll talk about the mission, vision, goals and objectives, and what we hope to achieve
-              bother long term and short term.</p>
-            </td>
-            <td>
-              <h3>Strategies & Action Plans</h3>
-              <p>This is where we'll talk about further strategies and acton plans once the base has 
-              been setup. Hopefully I don't stuff this all up and I actually get it to work.</p>
-            </td>
-          </tr>
-        </table>
+        <div className="intro_text">
+          <p>I'd love to hear from you if you had any questions or requests. Use the form below and I'll try and get back to you as soon
+          as I can!</p>
+        </div>
 
-        
+
+    <div className="form">
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+            <input className="name" type="text" value={this.state.name} onChange={this.handleChange} /><br />
+          E-mail:
+            <input className="email" type="text" value={this.state.email}/><br />
+          Message:
+            <input className="message" type="text" value={this.state.message}/><br />
+        </label>
+        <input className="button" type="submit" value="Submit" />
+      </form>
+    </div>
+
       </div>
+
     );
   }
 }
